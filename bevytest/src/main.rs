fdcab131956366a8f0116af9ea09d9bf390e8a86
midstreeeam <midstream.lou@gmail.com>
2023-07-03 13:20:45 +0800

    joint sensor functions

--- bevytest/src/main.rs
+++ bevytest/src/main.rs
@@ -77,7 +77,7 @@ fn setup_physics(
             RigidBody::Dynamic,
             Velocity{
                 linvel: Vec2 { x: 0.0, y: 0.0 },
-                angvel: 0.0
+                angvel: 1.0
             },
         )).id();
 
@@ -101,8 +101,8 @@ fn setup_physics(
     let joint = RevoluteJointBuilder::new()
         .local_anchor1(Vec2::new(0.0, 0.0))
         .local_anchor2(Vec2::new(150.0, 0.0))
-        .motor(PI/2.0, 0.0, 10.0, 0.3)
-        .limits([0.0,3.0]);
+        // .motor(PI/2.0, 0.0, 10.0, 0.3)
+        .limits([-3.0,3.0]);
     
     commands.spawn((
         SpriteBundle{
@@ -117,15 +117,34 @@ fn setup_physics(
         RigidBody::Dynamic,
         Collider::cuboid(50.0, 25.0),
         ImpulseJoint::new(sq, joint),
+        Velocity{
+            linvel: Vec2 { x: 0.0, y: 0.0 },
+            angvel: 0.0
+        }
     ));
 }
 
-fn print_angle(joints: Query<&ImpulseJoint>){
-    for joint in joints.iter(){
-        // println!("{:#?}",joint)
+fn print_angle(
+    joints: Query<(&Transform, &ImpulseJoint, &Velocity)>,
+    tvq: Query<(&Transform, &Velocity)>
+){
+    for (child_transform,joint, child_v) in joints.iter(){
+        if let Ok((parent_transform,parent_v)) = tvq.get(joint.parent) {
+            print!("{:#?}   ",get_relative_rotation(parent_transform, child_transform));
+            println!("{:#?}",get_relative_angular_velocity(parent_v, child_v));
+        }
     }
 }
 
+fn get_relative_rotation(transform1: &Transform, transform2: &Transform) -> f32{
+    let r1 = transform1.rotation;
+    let r2 = transform2.rotation;
+    r1.z.atan2(r1.w)*360.0/PI-r2.z.atan2(r2.w)*360.0/PI
+}
+
+fn get_relative_angular_velocity(v1: &Velocity, v2:&Velocity) ->f32{
+    (v1.angvel-v2.angvel)/PI*180.0
+}
 // #[derive(SystemParam)]
 // struct Myhook{}
 
