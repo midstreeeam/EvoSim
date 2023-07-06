f09334c73f00b61b2afddd133426c0897f98b9a6
midstreeeam <midstream.lou@gmail.com>
2023-07-06 16:27:58 +0800

    consts

---
+++ src/consts.rs
@@ -0,0 +1,17 @@
+
+// scale world size
+pub const WORLD_WIDTH:f32 = 10000.0;
+pub const WORLD_HEIGHT:f32 = 10000.0;
+
+// joint config
+pub const MOTOR_STIFFNESS:f32 = 10.0;
+pub const MOTOR_DAMPING:f32 = 0.0;
+pub const ENABLE_CONTACTS:bool = false;
+
+
+// math
+pub const EPSILON:f32 = 0.0001; // max error
+
+
+// physics
+pub const DRAG_COEFF:f32 = 5.0; // drag coefficient in fluid simulation
